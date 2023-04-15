022535ebf523a89742ed2ad28199c59b0a467e29
Marco Zocca <marco.zocca@unfoldml.com>
2023-04-15 22:20:01 +0200

    saving

--- src/Text/XML/Lens/Micro.hs
+++ src/Text/XML/Lens/Micro.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
 {-# language ExistentialQuantification #-}
+{-# language OverloadedStrings #-}
 {-# language RecordWildCards #-}
 {-# language Rank2Types #-}
 {-# options_ghc -Wno-unused-imports #-}
 module Text.XML.Lens.Micro (
-  -- * Traversals
+  root,
+  epilogue,
   named,
   nodes,
   -- ** node attribute combinators
@@ -13,26 +16,45 @@ module Text.XML.Lens.Micro (
   attributeIs,
   withoutAttribute,
   -- * prisms
-  previewP,
-  _Element,
-  _Content,
-  Prism'
+  -- previewP,
+  -- _Element,
+  -- _Content,
+  -- Prism',
+  -- ** no-prism
+  -- nodeElement,
+  -- nodeContent
                            ) where
 
+import Control.Exception (SomeException)
 import Data.Maybe (isNothing)
 import Data.Monoid (First(..))
 
 -- case-insensitive
 import qualified Data.CaseInsensitive as CI
 -- containers
-import Data.Map (Map)
+import Data.Map (Map, fromList)
+-- data-default
+import Data.Default.Class (Default(..))
 -- microlens
-import Lens.Micro.GHC (Getting, Lens', Traversal', ix, filtered)
+import Lens.Micro.GHC (Getting, Lens', (^.), (^..), SimpleFold, Traversal, Traversal', ix, filtered)
 import Lens.Micro.Extras (preview)
 -- text
 import Data.Text (Text)
+import qualified Data.Text.Lazy as TL (Text)
 -- xml-conduit
-import Text.XML (Element(..), Name(..), Node(..))
+import Text.XML (parseText, Prologue(..), Doctype(..), Document(..), Element(..), Name(..), Node(..), Miscellaneous(..))
+
+
+
+
+-- | The root element of the document.
+root :: Lens' Document Element
+root f doc = fmap (\p -> doc { documentRoot = p} ) $ f $ documentRoot doc
+{-# INLINE root #-}
+
+epilogue :: Lens' Document [Miscellaneous]
+epilogue f doc = fmap (\p -> doc { documentEpilogue = p} ) $ f $ documentEpilogue doc
+{-# INLINE epilogue #-}
 
 -- | Traverse elements which has the specified *local* name (case-insensitive).
 named :: CI.CI Text -> Traversal' Element Element
@@ -72,39 +94,74 @@ attributeIs n v = attributeSatisfies n (==v)
 {-# INLINE attributeIs #-}
 
 
--- from https://blog.jle.im/entry/lenses-products-prisms-sums.html#through-the-looking-prism
-data Prism' s a = forall q. Prism'
-    { match  :: s -> Either a q
-    , inject :: Either a q -> s
-    }
-
--- | Focus on node elements
-_Element :: Prism' Node Element
-_Element = Prism' {
-  match = \case
-      NodeElement e -> Left e
-      i -> Right i
-  , inject = \case
-      Left e -> NodeElement e
-      Right i -> i
-                  }
-
--- | Focus on the text content of nodes
-_Content :: Prism' Node Text
-_Content = Prism' {
-  match = \case
-      NodeContent c -> Left c
-      i -> Right i
-  , inject = \case
-      Left c -> NodeContent c
-      Right i -> i
-                  }
 
 
 
--- | 'preview' for 'Prism''
-previewP :: Prism' s a -> (s -> Maybe a)
-previewP Prism'{..} x = case match x of
-    Left  y -> Just y
-    Right _ -> Nothing
+
+
+
+
+-- t0 :: TL.Text
+-- t0 = "<!DOCTYPE html><html><head><title>Page Title</title></head><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>"
+-- t0e :: Either SomeException Document
+-- t0e = parseText def t0
+
+-- doc :: Document
+-- doc = Document {documentPrologue = Prologue {prologueBefore = [], prologueDoctype = Just (Doctype {doctypeName = "html", doctypeID = Nothing}), prologueAfter = []}, documentRoot = Element {elementName = Name {nameLocalName = "html", nameNamespace = Nothing, namePrefix = Nothing}, elementAttributes = fromList [], elementNodes = [NodeElement (Element {elementName = Name {nameLocalName = "head", nameNamespace = Nothing, namePrefix = Nothing}, elementAttributes = fromList [], elementNodes = [NodeElement (Element {elementName = Name {nameLocalName = "title", nameNamespace = Nothing, namePrefix = Nothing}, elementAttributes = fromList [], elementNodes = [NodeContent "Page Title"]})]}),NodeElement (Element {elementName = Name {nameLocalName = "body", nameNamespace = Nothing, namePrefix = Nothing}, elementAttributes = fromList [], elementNodes = [NodeElement (Element {elementName = Name {nameLocalName = "h1", nameNamespace = Nothing, namePrefix = Nothing}, elementAttributes = fromList [], elementNodes = [NodeContent "My First Heading"]}),NodeElement (Element {elementName = Name {nameLocalName = "p", nameNamespace = Nothing, namePrefix = Nothing}, elementAttributes = fromList [], elementNodes = [NodeContent "My first paragraph."]})]})]}, documentEpilogue = []}
+
+
+
+
+
+
+
+
+
+
+
+
+-- nodeElement :: (Element -> Maybe Element) -> Node -> Maybe Node
+-- nodeElement f = \case
+--   NodeElement e -> NodeElement <$> f e
+--   _ -> Nothing
+
+-- nodeContent :: (Text -> Maybe Text) -> Node -> Maybe Node
+-- nodeContent f = \case
+--   NodeContent c -> NodeContent <$> f c
+--   _ -> Nothing
+
+-- -- from https://blog.jle.im/entry/lenses-products-prisms-sums.html#through-the-looking-prism
+-- data Prism' s a = forall q. Prism'
+--     { match  :: s -> Either a q
+--     , inject :: Either a q -> s
+--     }
+
+-- -- | Focus on node elements
+-- _Element :: Prism' Node Element
+-- _Element = Prism' {
+--   match = \case
+--       NodeElement e -> Left e
+--       i -> Right i
+--   , inject = \case
+--       Left e -> NodeElement e
+--       Right i -> i
+--                   }
+
+-- -- | Focus on the text content of nodes
+-- _Content :: Prism' Node Text
+-- _Content = Prism' {
+--   match = \case
+--       NodeContent c -> Left c
+--       i -> Right i
+--   , inject = \case
+--       Left c -> NodeContent c
+--       Right i -> i
+--                   }
+
+
+-- -- | 'preview' for 'Prism''
+-- previewP :: Prism' s a -> (s -> Maybe a)
+-- previewP Prism'{..} x = case match x of
+--     Left  y -> Just y
+--     Right _ -> Nothing
 
